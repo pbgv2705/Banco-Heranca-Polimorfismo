484e66da40a94a383f0399d60be471059a471769
Paula Gimenez <paulaherbal@gmail.com>
2022-01-18 16:02:42 -0300

    Exercicio Banco - Herança e Polimorfismo

---
+++ Contas-Heranca-e-Polimorfismo/Entities/SavingsAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contas_Heranca_e_Polimorfismo.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+        public override void Withdraw(double amount)
+        {
+            // Balance -= amount;  Saque sem taxa
+            base.Withdraw(amount);  // Saque com taxa original de R$ 5.0 + R$ 2.0
+            Balance -= 2.0;
+        }
+        
+    }
+}
---
+++ Contas-Heranca-e-Polimorfismo/Program.cs
@@ -0,0 +1,50 @@
+﻿using Contas_Heranca_e_Polimorfismo.Entities;
+using System;
+
+namespace Contas_Heranca_e_Polimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(1001, "Alex", 500.0);
+            BusinessAccount bacc = new BusinessAccount(2001, "Maria", 0.0, 500.0);
+
+            // UPCASTING --> Conversão de subclasse para superclasse
+
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(2003, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(3001, "Anna", 500.0, 0.01);
+
+            // DOWNCASTING --> Conversão de superclasse para subclasse
+
+            BusinessAccount acc4 = (BusinessAccount)acc2; // --> Preciso fazer a conversão
+            acc4.Loan(100.0);
+
+            // BusinessAccount acc5 = (BusinessAccount)acc3; --> Tipos incompativeis. Da erro
+
+            if (acc3 is BusinessAccount)
+            {
+                //BusinessAccount acc5 = (BusinessAccount)acc3;
+                BusinessAccount acc5 = acc3 as BusinessAccount; // outra forma de fazer o casting
+                acc5.Loan(100.0);
+                System.Console.WriteLine("Loan!");
+            }
+
+            if (acc3 is SavingsAccount)
+            {
+                // SavingsAccount acc5 = (SavingsAccount)acc3;
+                SavingsAccount acc5 = acc3 as SavingsAccount;
+                acc5.UpdateBalance();
+                System.Console.WriteLine("Update!");
+            }
+
+            acc.Withdraw(10.0);
+            acc3.Withdraw(10.0);
+            Console.WriteLine(acc.Balance);
+            Console.WriteLine(acc3.Balance);
+        }
+
+        
+    }
+}
